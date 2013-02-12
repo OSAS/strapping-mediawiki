b1cc49cb0ff7363232ee11f6dcfd21e185b83692
Ryan Lane <rlane@wikimedia.org>
2013-02-12 14:45:45 -0800

    Changing extension credit url to point to github
    The url was accidentally left as a boiler plate. This change adds
    the github url.

--- strapping.php
+++ strapping.php
@@ -12,7 +12,7 @@ if( !defined( 'MEDIAWIKI' ) ) die( "This is an extension to the MediaWiki packag
 $wgExtensionCredits['skin'][] = array(
         'path' => __FILE__,
         'name' => 'Strapping',
-        'url' => "[...]",
+        'url' => "https://github.com/OSAS/strapping-mediawiki",
         'author' => 'Garrett LeSage',
         'descriptionmsg' => 'strapping-desc',
 );
