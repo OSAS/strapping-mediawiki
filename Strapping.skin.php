bc7194724a2d5bda2653e175b3fd659c739c4d8c
Ryan Lane <rlane@wikimedia.org>
2013-02-11 16:25:45 -0800

    Remove debugging calls and commented output
    Removing any commented out code. It can be added back in if needed.

--- Strapping.skin.php
+++ Strapping.skin.php
@@ -21,13 +21,6 @@ class SkinStrapping extends SkinTemplate {
   var $skinname = 'strapping', $stylename = 'strapping',
     $template = 'StrappingTemplate', $useHeadElement = true;
 
-  function tocList($toc) {
-    $this->savedToc = parent::tocList($toc);
-
-    #return "<section class='mytocthing'>" . $this-savedToc . "</section>";
-    return "";
-  }
-
   /**
    * Initializes output page and sets up skin-specific parameters
    * @param $out OutputPage object to initialize
@@ -125,17 +118,6 @@ class StrappingTemplate extends BaseTemplate {
     $this->data['action_urls'] = $nav['actions'];
     $this->data['variant_urls'] = $nav['variants'];
 
-    // Reverse horizontally rendered navigation elements
-    # We're changing theese vertically, 
-    # so flipping them shouldn't be necessary
-    if ( $this->data['rtl'] ) {
-      #$this->data['view_urls'] =
-        #array_reverse( $this->data['view_urls'] );
-      #$this->data['namespace_urls'] =
-        #array_reverse( $this->data['namespace_urls'] );
-      #$this->data['personal_urls'] =
-        #array_reverse( $this->data['personal_urls'] );
-    }
     // Output HTML Page
     $this->html( 'headelement' );
 ?>
@@ -164,9 +146,6 @@ class StrappingTemplate extends BaseTemplate {
 
       <div class="pull-right">
         <?php
-          # Namespaces, views, & variants have been merged into the page menu above
-          #$this->renderNavigation( array( 'NAMESPACES', 'VIEWS', 'VARIANTS' ) ); 
-
           if ($wgSearchPlacement['header']) {
             $this->renderNavigation( array( 'SEARCH' ) ); 
           }
@@ -179,22 +158,13 @@ class StrappingTemplate extends BaseTemplate {
     </div>
   </div>
 </div>
-<!--
-<pre><?php print_r($this->getToolbox()); ?></pre>
-<pre><?php print_r($this->data['content_actions']); ?></pre>
-<pre><?php #print_r($this->data); ?></pre>
--->
 <?php } ?>
-<!--<pre><?php print_r($this->getPersonalTools()); ?></pre>-->
-
 
     <div id="mw-page-base" class="noprint"></div>
     <div id="mw-head-base" class="noprint"></div>
 
     <!-- Header -->
     <div id="page-header" class="container">
-      <section class="row">
-
       <section class="span12">
 
         <!-- logo -->
@@ -243,19 +213,9 @@ class StrappingTemplate extends BaseTemplate {
       <div id="siteNotice"><?php $this->html( 'sitenotice' ) ?></div>
       <!-- /sitenotice -->
       <?php endif; ?>
-      <!-- firstHeading -->
-      <!--
-      <h1 id="firstHeading" class="firstHeading page-header">
-        <span dir="auto"><?php $this->html( 'title' ) ?></span>
-      </h1>
-      -->
-      <!-- /firstHeading -->
       <!-- bodyContent -->
       <div id="bodyContent">
         <?php if ( $this->data['isarticle'] ): ?>
-        <!-- tagline -->
-        <!--<div id="siteSub"><?php $this->msg( 'tagline' ) ?></div>-->
-        <!-- /tagline -->
         <?php endif; ?>
         <!-- subtitle -->
         <div id="contentSub"<?php $this->html( 'userlangattributes' ) ?>><?php $this->html( 'subtitle' ) ?></div>
