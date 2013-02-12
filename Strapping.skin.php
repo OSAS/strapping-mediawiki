36607ee65807bf5934395ebd43e18f664ac63693
Ryan Lane <rlane@wikimedia.org>
2013-02-11 19:18:26 -0800

    Last time. Properly fix undefined index error.

--- Strapping.skin.php
+++ Strapping.skin.php
@@ -636,10 +636,7 @@ class StrappingTemplate extends BaseTemplate {
             foreach( $content as $key => $val ) {
               $navClasses = '';
 
-              if ( !array_key_exists('view', $this->data['content_navigation']['views']) ) {
-                continue;
-              }
-              if ($this->data['content_navigation']['views']['view']['href'] == $val['href']) {
+              if (array_key_exists('view', $this->data['content_navigation']['views'] && $this->data['content_navigation']['views']['view']['href'] == $val['href']) {
                 $navClasses = 'active';
               }?>
 
