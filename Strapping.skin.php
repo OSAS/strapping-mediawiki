1d2e2d241fde0f417726117360a73404cea461c2
Garrett LeSage <garrett@lesage.us>
2013-06-13 17:08:33 +0200

    Check for $content using isset() instead of ! (issue #31)

--- Strapping.skin.php
+++ Strapping.skin.php
@@ -673,7 +673,7 @@ class StrappingTemplate extends BaseTemplate {
 
         case 'SIDEBAR':
           foreach ( $this->data['sidebar'] as $name => $content ) {
-            if ( !$content ) {
+            if ( isset($content) ) {
               continue;
             }
             if ( in_array( $name, $wgStrappingSkinSidebarItemsInNavbar ) ) {
