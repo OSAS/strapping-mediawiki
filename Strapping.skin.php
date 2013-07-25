4cb83dd40715764bbd0728453fcf56f0cad78e26
Garrett LeSage <garrett@lesage.us>
2013-07-25 13:22:40 +0200

    fixed style of non-article pages (such as editing, moving, etc.)

--- Strapping.skin.php
+++ Strapping.skin.php
@@ -260,9 +260,10 @@ class StrappingTemplate extends BaseTemplate {
         <!-- /jumpto -->
         <?php endif; ?>
 
+
         <!-- innerbodycontent -->
-        <?php # Peek into the body content, to see if a custom layout is used
-        if ($wgStrappingSkinUseStandardLayout || preg_match("/class.*row/i", $this->data['bodycontent'])) { 
+        <?php # Peek into the body content of articles, to see if a custom layout is used
+        if ($wgStrappingSkinUseStandardLayout || preg_match("/class.*row/i", $this->data['bodycontent']) && $this->data['articleid']) {
           # If there's a custom layout, the H1 and layout is up to the page ?>
           <div id="innerbodycontent" class="layout">
             <h1 id="firstHeading" class="firstHeading page-header">
