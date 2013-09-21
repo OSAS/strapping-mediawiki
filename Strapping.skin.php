1f494b86cf35dabe226df68c1a252328a43e2f55
Xander Miller <xander.miller@gmail.com>
2013-09-21 14:07:02 -0400

    Increase percision on RegEx statement that was triggering on wiki content if it contained the string 'class' followed by the string 'row' eg. 'Middle class borrowing has increased'.

--- Strapping.skin.php
+++ Strapping.skin.php
@@ -263,7 +263,7 @@ class StrappingTemplate extends BaseTemplate {
 
         <!-- innerbodycontent -->
         <?php # Peek into the body content of articles, to see if a custom layout is used
-        if ($wgStrappingSkinUseStandardLayout || preg_match("/class.*row/i", $this->data['bodycontent']) && $this->data['articleid']) {
+        if ($wgStrappingSkinUseStandardLayout || preg_match("/<div.*class.*row.*>/i", $this->data['bodycontent']) && $this->data['articleid']) {
           # If there's a custom layout, the H1 and layout is up to the page ?>
           <div id="innerbodycontent" class="layout">
             <h1 id="firstHeading" class="firstHeading page-header">
